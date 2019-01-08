1509f38481c2a5fecaa35d9ee9bbaa6a45a0a95d
Marc Zöller <m.zoeller@usu.de>
2019-01-08 14:01:05 +0100

    Implement more synthetic functions

--- benchmark/synthetic.py
+++ benchmark/synthetic.py
@@ -1,12 +1,41 @@
 import numpy as np
 from hpolib.abstract_benchmark import AbstractBenchmark
-from hpolib.benchmarks.synthetic_functions import Branin, Bohachevsky, Camelback, Forrester, GoldsteinPrice
+from hpolib.benchmarks import synthetic_functions
 
 from benchmark.base import _dict_as_array
 from config import BaseConverter, MetaConfig, NoopConverter
 
 
-class BraninFunction(Branin):
+class Bohachevsky(synthetic_functions.Bohachevsky):
+
+    @_dict_as_array
+    @AbstractBenchmark._configuration_as_array
+    def objective_function(self, x, **kwargs):
+        y = 0.7 + x[0] ** 2 + 2.0 * x[1] ** 2
+        y -= 0.3 * np.cos(3.0 * np.pi * x[0])
+        y -= 0.4 * np.cos(4.0 * np.pi * x[1])
+
+        return {'function_value': y}
+
+    @staticmethod
+    def get_configuration_space(converter: BaseConverter = NoopConverter()):
+        cs = {
+            "x0": {
+                "type": "uniform_float",
+                "lower": -100,
+                "upper": 100
+            },
+            "x1": {
+                "type": "uniform_float",
+                "lower": -100,
+                "upper": 100
+            }
+        }
+        return converter.convert_single(MetaConfig(cs))
+
+
+class Branin(synthetic_functions.Branin):
+
     @_dict_as_array
     @AbstractBenchmark._configuration_as_array
     def objective_function(self, x, **kwargs):
@@ -21,12 +50,12 @@ class BraninFunction(Branin):
     @staticmethod
     def get_configuration_space(converter: BaseConverter = NoopConverter()):
         cs = {
-            "x1": {
+            "x0": {
                 "type": "uniform_float",
                 "lower": -5,
                 "upper": 10
             },
-            "x2": {
+            "x1": {
                 "type": "uniform_float",
                 "lower": 0,
                 "upper": 15
@@ -35,35 +64,7 @@ class BraninFunction(Branin):
         return converter.convert_single(MetaConfig(cs))
 
 
-class BohachevskyFunction(Bohachevsky):
-
-    @_dict_as_array
-    @AbstractBenchmark._configuration_as_array
-    def objective_function(self, x, **kwargs):
-        y = 0.7 + x[0] ** 2 + 2.0 * x[1] ** 2
-        y -= 0.3 * np.cos(3.0 * np.pi * x[0])
-        y -= 0.4 * np.cos(4.0 * np.pi * x[1])
-
-        return {'function_value': y}
-
-    @staticmethod
-    def get_configuration_space(converter: BaseConverter = NoopConverter()):
-        cs = {
-            "x1": {
-                "type": "uniform_float",
-                "lower": -100,
-                "upper": 100
-            },
-            "x2": {
-                "type": "uniform_float",
-                "lower": -100,
-                "upper": 100
-            }
-        }
-        return converter.convert_single(MetaConfig(cs))
-
-
-class CamelbackFunction(Camelback):
+class Camelback(synthetic_functions.Camelback):
 
     @_dict_as_array
     @AbstractBenchmark._configuration_as_array
@@ -75,12 +76,12 @@ class CamelbackFunction(Camelback):
     @staticmethod
     def get_configuration_space(converter: BaseConverter = NoopConverter()):
         cs = {
-            "x1": {
+            "x0": {
                 "type": "uniform_float",
                 "lower": -5,
                 "upper": 5
             },
-            "x2": {
+            "x1": {
                 "type": "uniform_float",
                 "lower": -5,
                 "upper": 5
@@ -89,7 +90,7 @@ class CamelbackFunction(Camelback):
         return converter.convert_single(MetaConfig(cs))
 
 
-class ForresterFunction(Forrester):
+class Forrester(synthetic_functions.Forrester):
 
     @_dict_as_array
     @AbstractBenchmark._configuration_as_array
@@ -113,7 +114,7 @@ class ForresterFunction(Forrester):
         return converter.convert_single(MetaConfig(cs))
 
 
-class GoldsteinPriceFunction(GoldsteinPrice):
+class GoldsteinPrice(synthetic_functions.GoldsteinPrice):
 
     @_dict_as_array
     @AbstractBenchmark._configuration_as_array
@@ -128,15 +129,170 @@ class GoldsteinPriceFunction(GoldsteinPrice):
     @staticmethod
     def get_configuration_space(converter: BaseConverter = NoopConverter()):
         cs = {
-            "x1": {
+            "x0": {
                 "type": "uniform_float",
                 "lower": -2,
                 "upper": 2
             },
-            "x2": {
+            "x1": {
                 "type": "uniform_float",
                 "lower": -2,
                 "upper": 2
+            }
+        }
+        return converter.convert_single(MetaConfig(cs))
+
+
+class Hartmann3(synthetic_functions.Hartmann3):
+
+    @_dict_as_array
+    @AbstractBenchmark._configuration_as_array
+    def objective_function(self, x, **kwargs):
+        external_sum = 0
+        for i in range(4):
+            internal_sum = 0
+            for j in range(3):
+                internal_sum += self.A[i, j] * (x[j] - self.P[i, j]) ** 2
+            external_sum += self.alpha[i] * np.exp(-internal_sum)
+
+        return {'function_value': -external_sum}
+
+    @staticmethod
+    def get_configuration_space(converter: BaseConverter = NoopConverter()):
+        cs = {}
+        for i in range(3):
+            cs[f"x{i}"] = {
+                "type": "uniform_float",
+                "lower": 0,
+                "upper": 1
+            }
+        return converter.convert_single(MetaConfig(cs))
+
+
+class Hartmann6(synthetic_functions.Hartmann6):
+
+    @_dict_as_array
+    @AbstractBenchmark._configuration_as_array
+    def objective_function(self, x, **kwargs):
+        """6d Hartmann test function
+            input bounds:  0 <= xi <= 1, i = 1..6
+            global optimum: (0.20169, 0.150011, 0.476874, 0.275332, 0.311652, 0.6573),
+            min function value = -3.32237
+        """
+
+        external_sum = 0
+        for i in range(4):
+            internal_sum = 0
+            for j in range(6):
+                internal_sum += self.A[i, j] * (x[j] - self.P[i, j]) ** 2
+            external_sum += self.alpha[i] * np.exp(-internal_sum)
+
+        return {'function_value': -external_sum}
+
+    @staticmethod
+    def get_configuration_space(converter: BaseConverter = NoopConverter()):
+        cs = {}
+        for i in range(6):
+            cs[f"x{i}"] = {
+                "type": "uniform_float",
+                "lower": 0,
+                "upper": 1
+            }
+        return converter.convert_single(MetaConfig(cs))
+
+
+class Levy(synthetic_functions.Levy):
+
+    @_dict_as_array
+    @AbstractBenchmark._configuration_as_array
+    def objective_function(self, x, **kwargs):
+        z = 1 + ((x[0] - 1.) / 4.)
+        s = np.power((np.sin(np.pi * z)), 2)
+        y = (s + ((z - 1) ** 2) * (1 + np.power((np.sin(2 * np.pi * z)), 2)))
+
+        return {'function_value': y}
+
+    @staticmethod
+    def get_configuration_space(converter: BaseConverter = NoopConverter()):
+        cs = {
+            "x": {
+                "type": "uniform_float",
+                "lower": -15,
+                "upper": 10
+            }
+        }
+        return converter.convert_single(MetaConfig(cs))
+
+
+# Rosenbrock2D, Rosenbrock5D and Rosenbrock10D are not implemented
+
+class Rosenbrock20D(synthetic_functions.rosenbrock.Rosenbrock20D):
+
+    @_dict_as_array
+    @AbstractBenchmark._configuration_as_array
+    def objective_function(self, x, **kwargs):
+        y = 0
+        d = 20
+        for i in range(d - 1):
+            y += 100 * (x[i + 1] - x[i] ** 2) ** 2
+            y += (x[i] - 1) ** 2
+
+        return {'function_value': y}
+
+    @staticmethod
+    def get_configuration_space(converter: BaseConverter = NoopConverter()):
+        cs = {}
+        for i in range(20):
+            cs[f"x{i}"] = {
+                "type": "uniform_float",
+                "lower": 0,
+                "upper": 1
+            }
+        return converter.convert_single(MetaConfig(cs))
+
+
+class SinOne(synthetic_functions.SinOne):
+
+    @_dict_as_array
+    @AbstractBenchmark._configuration_as_array
+    def objective_function(self, x, **kwargs):
+        y = 0.5 * np.sin(13 * x[0]) * np.sin(27 * x[0]) + 0.5
+
+        return {'function_value': y}
+
+    @staticmethod
+    def get_configuration_space(converter: BaseConverter = NoopConverter()):
+        cs = {
+            "x": {
+                "type": "uniform_float",
+                "lower": 0,
+                "upper": 1
+            }
+        }
+        return converter.convert_single(MetaConfig(cs))
+
+
+class SinTwo(synthetic_functions.SinTwo):
+
+    @_dict_as_array
+    @AbstractBenchmark._configuration_as_array
+    def objective_function(self, x, **kwargs):
+        y = (0.5 * np.sin(13 * x[0]) * np.sin(27 * x[0]) + 0.5) * (0.5 * np.sin(13 * x[1]) * np.sin(27 * x[1]) + 0.5)
+
+        return {'function_value': y}
+
+    @staticmethod
+    def get_configuration_space(converter: BaseConverter = NoopConverter()):
+        cs = {
+            "x0": {
+                "type": "uniform_float",
+                "lower": 0,
+                "upper": 1
+            },
+            "x1": {
+                "type": "uniform_float",
+                "lower": 0,
+                "upper": 1
             }
         }
         return converter.convert_single(MetaConfig(cs))
--- evaluation/synthetic.py
+++ evaluation/synthetic.py
@@ -16,22 +16,22 @@ config_dict = {
 }
 config = Namespace(**config_dict)
 
-benchmark = benchmark.BraninFunction()
+benchmark = benchmark.Branin()
 
 # Random Search
 if config.random_search:
     rs = ObjectiveRandomSearch(config.timeout, config.n_jobs, random_state=config.seed)
     stats = rs.optimize(benchmark)
-    print(stats.evaluations)
+    print(len(stats.evaluations))
 
 # Grid Search
 if config.grid_search:
     rs = ObjectiveGridSearch(config.timeout, config.n_jobs)
     stats = rs.optimize(benchmark)
-    print(stats.evaluations)
+    print(len(stats.evaluations))
 
 # SMAC
 if config.smac:
     smac = SmacAdapter(config.timeout, config.n_jobs, config.seed)
     stats = smac.optimize(benchmark)
-    print(stats.evaluations)
+    print(len(stats.evaluations))
