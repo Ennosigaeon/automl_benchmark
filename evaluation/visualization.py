09d74a79ce51e1bda9804a3f89c432f117967c50
Marc Zöller <m.zoeller@usu.de>
2019-02-09 14:48:53 +0100

    Add plot of tested configurations

--- evaluation/visualization.py
+++ evaluation/visualization.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,7 +9,7 @@ from adapter.base import BenchmarkResult
 from evaluation.base import MongoPersistence
 
 
-def plot_results(benchmark_result: BenchmarkResult):
+def plot_evluation_performance(benchmark_result: BenchmarkResult):
     fig, ax = plt.subplots()
     fig.set_size_inches(16, 9)
     fig.set_dpi(250)
@@ -24,17 +25,17 @@ def plot_results(benchmark_result: BenchmarkResult):
         ax.plot(x, y, label=res.algorithm)
 
     ax.set_xscale('log')
-    ax.set_yscale('log')
+    # ax.set_yscale('log')
     ax.legend(loc='upper right')
     ax.set_title(benchmark_result.name)
     ax.set_xlabel('Iteration')
 
     plt.savefig('{}.pdf'.format(benchmark_result.name), bbox_inches="tight")
-    fig.show()
-    plt.show()
+    # fig.show()
+    # plt.show()
 
 
-def plot_mulitple_results(ls: List[BenchmarkResult]):
+def plot_surrogate_performance(ls: List[BenchmarkResult]):
     benchmark = ls[0].benchmark
 
     fig, ax = plt.subplots()
@@ -61,6 +62,12 @@ def plot_mulitple_results(ls: List[BenchmarkResult]):
         ax.fill_between(np.arange(0, y.shape[1], 1), mean - std, mean + std, alpha=0.25)
         ax.plot(mean, label=name)
 
+    # Fixes for skewed plots
+    if ls[0].name == 'Branin':
+        ax.set_ylim(-10, 90)
+    if ls[0].name == 'Rosenbrock10D':
+        ax.set_ylim(-10, 270)
+
     ax.set_xscale('log')
     # ax.set_yscale('log')
     ax.legend(loc='upper right')
@@ -68,18 +75,64 @@ def plot_mulitple_results(ls: List[BenchmarkResult]):
     ax.set_xlabel('Iteration')
 
     plt.savefig('{}_aggregated.pdf'.format(ls[0].name), bbox_inches="tight")
-    fig.show()
-    plt.show()
+    # fig.show()
+    # plt.show()
+
+
+def plot_evaluated_configurations(ls: List[BenchmarkResult]):
+    benchmark = ls[0].benchmark
+    res = ls[0]
+
+    bounds = benchmark.get_meta_information()['bounds']
+    grid = []
+    for bound in bounds:
+        grid.append(np.linspace(bound[0], bound[1], 100))
+
+    x = grid[0]
+    y = grid[1]
+    yy, xx = np.meshgrid(y, x)
+
+    zz = np.zeros(xx.shape)
+    for i in range(len(x)):
+        for j in range(len(y)):
+            zz[i][j] = benchmark.objective_function([x[i], y[j]])['function_value']
+
+    fig, ax = plt.subplots()
+
+    # Create color mesh
+    im = plt.pcolormesh(xx, yy, zz, norm=colors.LogNorm(vmin=zz.min(), vmax=zz.max()))
+    cbar = fig.colorbar(im, ax=ax)
+    cbar.ax.set_ylabel('Objective Function', rotation=90)
+
+    c = ['r', 'k', 'm']
+    for idx, solver in enumerate([s for s in res.solvers if s.algorithm in ['Grid Search', 'Random Search', 'BOHB']]):
+        x = np.zeros(len(solver.evaluations))
+        y = np.zeros(len(solver.evaluations))
+        for i in range(len(solver.evaluations)):
+            x[i] = solver.evaluations[i].config['x0']
+            y[i] = solver.evaluations[i].config['x1']
+        ax.scatter(x, y, s=10, c=c[idx], label=solver.algorithm)
+
+    ax.legend(loc='upper right')
+    ax.set_xlabel('$x_0$')
+    ax.set_ylabel('$x_1$')
+    fig.tight_layout()
+
+    plt.savefig('{}_tested_configurations.pdf'.format(ls[0].name), bbox_inches="tight")
+    # fig.show()
+    # plt.show()
 
 
 if __name__ == '__main__':
     persistence = MongoPersistence('10.0.2.2')
 
-    ls = [benchmark.Levy(), benchmark.Branin(), benchmark.Hartmann3(), benchmark.Hartmann6(), benchmark.Rosenbrock10D(),
-          benchmark.Rosenbrock20D()]
-    for b in ls:
-        res = persistence.load_all(b)
-        plot_mulitple_results(res)
+    # ls = [benchmark.Levy(), benchmark.Branin(), benchmark.Hartmann6(), benchmark.Rosenbrock10D()]
+    # for b in ls:
+    #     res = persistence.load_all(b)
+    #     plot_surrogate_performance(res)
 
     # res = persistence.load_single(benchmark.Levy())
     # plot_results(res)
+
+    res = persistence.load_all(benchmark.Branin())
+    plot_evaluated_configurations(res)
