2df36731e6ba00f670fa9f2b338174451db15b32
Marc Zöller <m.zoeller@usu.de>
2019-03-07 22:23:29 +0100

    Add random search to auto-sklearn

--- run_auto_sklearn.py
+++ run_auto_sklearn.py
@@ -9,14 +9,32 @@ import sklearn.model_selection
 from autosklearn.classification import AutoSklearnClassifier
 from autosklearn.constants import *
 from autosklearn.metrics import accuracy
+from smac.facade.roar_facade import ROAR
+from smac.scenario.scenario import Scenario
 
 from benchmark import OpenMLBenchmark
 
 timeout = 3600
 run_timeout = 360
 jobs = 8
+random = False
 
 ensemble_size = 20
+
+
+def get_random_search_object_callback(scenario_dict, seed, ta, backend, metalearning_configurations, runhistory):
+    """Random search."""
+    scenario_dict['input_psmac_dirs'] = backend.get_smac_output_glob()
+    scenario_dict['minR'] = len(scenario_dict['instances'])
+    scenario_dict['initial_incumbent'] = 'RANDOM'
+    scenario = Scenario(scenario_dict)
+    return ROAR(
+        scenario=scenario,
+        rng=seed,
+        tae_runner=ta,
+        runhistory=runhistory,
+        run_id=seed
+    )
 
 
 def get_spawn_classifier(X_train, y_train, tmp_folder, output_folder):
@@ -30,6 +48,10 @@ def get_spawn_classifier(X_train, y_train, tmp_folder, output_folder):
         else:
             initial_configurations_via_metalearning = 0
             smac_scenario_args = {'initial_incumbent': 'RANDOM'}
+
+        callback = None
+        if random:
+            callback = get_random_search_object_callback
 
         # Arguments which are different to other runs of auto-sklearn:
         # 1. all classifiers write to the same output directory
@@ -47,8 +69,10 @@ def get_spawn_classifier(X_train, y_train, tmp_folder, output_folder):
             initial_configurations_via_metalearning=initial_configurations_via_metalearning,
             seed=seed,
             smac_scenario_args=smac_scenario_args,
+            get_smac_object_callback=callback
         )
         automl.fit(X_train, y_train, dataset_name=dataset_name)
+        print(automl.sprint_statistics())
 
     return spawn_classifier
 
@@ -95,7 +119,7 @@ def main(bm: OpenMLBenchmark):
 
     predictions = automl.predict(X_test)
     print(automl.show_models())
-    print('Accuracy score', sklearn.metrics.accuracy_score(y_test, predictions))
+    print('Misclassification rate', 1 - sklearn.metrics.accuracy_score(y_test, predictions))
 
 
 if __name__ == '__main__':
@@ -103,6 +127,10 @@ if __name__ == '__main__':
         shutil.rmtree('tmp/autosklearn/')
     except OSError as e:
         pass
+
+    print('Timeout: ', timeout)
+    print('Run Timeout: ', run_timeout)
+    print('Random Search: ', random)
 
     task_ids = [22, 37, 2079, 3543, 3899, 3913, 3917, 9950, 9980, 14966]
     for task in task_ids:
