0204bc1e9b7e273653ca0e3775b349392d55452a
Marc Zoeller <m.zoeller@usu.de>
2019-10-10 09:12:26 +0200

    Change order of for loops

--- run_hpsklearn.py
+++ run_hpsklearn.py
@@ -37,22 +37,21 @@ def main(bm: OpenMLBenchmark):
 
 
 if __name__ == '__main__':
-    for i in range(10):
-        print('#######\nIteration {}\n#######'.format(i))
-        print('Timeout: ', timeout)
-        print('Run Timeout: ', run_timeout)
+    print('Timeout: ', timeout)
+    print('Run Timeout: ', run_timeout)
 
-        task_ids = [3, 12, 31, 53, 3917, 3945, 7593, 9952, 9977, 9981, 10101, 14965, 34539, 146195, 146212, 146818,
-                    146821, 146822, 146825, 167119, 167120, 168329, 168330, 168331, 168332, 168335, 168337, 168338,
-                    168868, 168908, 168909, 168910, 168911, 168912, 189354, 189355, 189356]
-        for task in task_ids:
-            print('Starting task {} at {}'.format(task, datetime.datetime.now().time()))
+    task_ids = [3, 12, 31, 53, 3917, 3945, 7593, 9952, 9977, 9981, 10101, 14965, 34539, 146195, 146212, 146818,
+                146821, 146822, 146825, 167119, 167120, 168329, 168330, 168331, 168332, 168335, 168337, 168338,
+                168868, 168908, 168909, 168910, 168911, 168912, 189354, 189355, 189356]
+    for task in task_ids:
+        print('#######Starting task {} at {}#######'.format(task, datetime.datetime.now().time()))
+        for i in range(10):
+            print('##\nIteration {}\n##'.format(i))
             bm = OpenMLBenchmark(task)
 
             for j in range(100):
                 print('Attempt {}...'.format(j))
                 try:
-
                     main(bm)
                     break
                 except Exception as e:
--- run_tpot.py
+++ run_tpot.py
@@ -29,17 +29,19 @@ def main(bm: OpenMLBenchmark):
 
 
 if __name__ == '__main__':
-    for i in range(10):
-        print('#######\nIteration {}\n#######'.format(i))
-        print('Timeout: ', timeout)
-        print('Run Timeout: ', run_timeout)
 
-        task_ids = [3, 12, 31, 53, 3917, 3945, 7593, 9952, 9977, 9981, 10101, 14965, 34539, 146195, 146212, 146818,
-                    146821, 146822, 146825, 167119, 167120, 168329, 168330, 168331, 168332, 168335, 168337, 168338,
-                    168868, 168908, 168909, 168910, 168911, 168912, 189354, 189355, 189356]
-        for task in task_ids:
+    print('Timeout: ', timeout)
+    print('Run Timeout: ', run_timeout)
+
+    task_ids = [3, 12, 31, 53, 3917, 3945, 7593, 9952, 9977, 9981, 10101, 14965, 34539, 146195, 146212, 146818,
+                146821, 146822, 146825, 167119, 167120, 168329, 168330, 168331, 168332, 168335, 168337, 168338,
+                168868, 168908, 168909, 168910, 168911, 168912, 189354, 189355, 189356]
+    for task in task_ids:
+        print('#######Starting task {} at {}#######'.format(task, datetime.datetime.now().time()))
+        for i in range(10):
             try:
-                print('Starting task {} at {}'.format(task, datetime.datetime.now().time()))
+                print('##\nIteration {}\n##'.format(i))
+
                 bm = OpenMLBenchmark(task)
                 main(bm)
             except Exception as e:
