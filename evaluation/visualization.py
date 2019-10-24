1d970168ec459d7f18cd7523ee15c59beacc6815
Marc Zoeller <m.zoeller@usu.de>
2019-10-24 14:42:54 +0200

    Commit old plot changes. Don't know what the changes exactly are...

--- evaluation/visualization.py
+++ evaluation/visualization.py
@@ -11,33 +11,7 @@ from benchmark import OpenML100Suite, OpenMLBenchmark
 from evaluation.base import MongoPersistence
 
 
-def plot_evaluation_performance(benchmark_result: BenchmarkResult):
-    fig, ax = plt.subplots()
-    fig.set_size_inches(16, 9)
-    fig.set_dpi(250)
-
-    f_opt = benchmark_result.benchmark.get_meta_information()['f_opt']
-    ax.plot([0, 100], [f_opt, f_opt], 'k', label='Optimum')
-
-    for res in benchmark_result.solvers:
-        x, y = res.as_numpy(incumbent=False, x_axis='iterations')
-        ax.plot(x, y, label=res.algorithm)
-
-        x, y = res.as_numpy(incumbent=True, x_axis='iterations')
-        ax.plot(x, y, label=res.algorithm)
-
-    ax.set_xscale('log')
-    # ax.set_yscale('log')
-    ax.legend(loc='upper right')
-    ax.set_title(benchmark_result.name)
-    ax.set_xlabel('Iteration')
-
-    plt.savefig('evaluation/plots/{}.pdf'.format(benchmark_result.name), bbox_inches='tight')
-    # fig.show()
-    # plt.show()
-
-
-def plot_incumbent_performance(ls: List[BenchmarkResult]):
+def plot_incumbent_performance(ls: List[BenchmarkResult], n: int = 250):
     benchmark = ls[0].benchmark
     matplotlib.rcParams.update({'font.size': 12})
 
@@ -51,31 +25,34 @@ def plot_incumbent_performance(ls: List[BenchmarkResult]):
             x, y = solver.as_numpy(incumbent=True, x_axis='iterations')
             label = solver.algorithm
             if label == 'RoBo gp':
-                label = 'RoBo'
-            solvers.setdefault(label, []).append(y)
+                label = 'RoBO'
+            solvers.setdefault(label, []).append(y[:n])
 
     f_opt = benchmark.get_meta_information()['f_opt']
-    ax.plot([1, len(next(iter(solvers.values()))[0]) + 1], [f_opt, f_opt], 'k', label='Optimum')
+    ax.plot([1, len(next(iter(solvers.values()))[0]) + 1], [f_opt, f_opt], 'k', label='Optimum', linewidth=2)
 
     for name, values in solvers.items():
         y = np.vstack(values)
-        mean = np.mean(y, axis=0)[:250]
-        std = np.std(y, axis=0)[:250]
+        mean = np.mean(y, axis=0)
+        std = np.std(y, axis=0)
 
-        ax.fill_between(np.arange(1, 251, 1), mean - std, mean + std, alpha=0.25)
-        ax.plot(np.arange(1, 251, 1), mean, label=name)
+        ax.fill_between(np.arange(1, n + 1, 1), mean - std, mean + std, alpha=0.25)
+        ax.plot(np.arange(1, n + 1, 1), mean, label=name, linewidth=2)
 
     # Fixes for skewed plots
     if ls[0].name == 'Branin':
         ax.set_ylim(-10, 90)
     if ls[0].name == 'Rosenbrock10D':
         ax.set_ylim(-10, 270)
+    if ls[0].name == 'Camelback':
+        # ax.set_yscale('log')
+        ax.set_ylim(0, 2000)
 
     ax.set_xscale('log')
-    # ax.set_yscale('log')
     ax.legend(loc='upper right')
     ax.set_title(ls[0].name)
     ax.set_xlabel('Iteration')
+    ax.set_ylabel('$f(\\lambda)$')
 
     plt.savefig('evaluation/plots/{}_aggregated.pdf'.format(ls[0].name), bbox_inches='tight')
     # fig.show()
@@ -134,6 +111,8 @@ def plot_evaluated_configurations(ls: List[BenchmarkResult]):
 
 
 def plot_method_overhead(ls: List[BenchmarkResult], line_plot: bool = True):
+    matplotlib.rcParams.update({'font.size': 11})
+
     fig, ax = plt.subplots()
     fig.set_size_inches(16, 9)
     fig.set_dpi(250)
@@ -146,9 +125,11 @@ def plot_method_overhead(ls: List[BenchmarkResult], line_plot: bool = True):
             if previous is not None:
                 y.append((ev.start - previous) * 1000)
             previous = ev.end
-        solvers.setdefault(solver.algorithm, []).append(y[:244])
 
-    solvers['RoBo'] = solvers.pop('RoBo gp')
+        key = solver.algorithm
+        if key == 'RoBo gp':
+            key = 'RoBO'
+        solvers.setdefault(key, []).append(y[:494])
 
     if line_plot:
         def smooth(y, box_pts):
@@ -162,7 +143,7 @@ def plot_method_overhead(ls: List[BenchmarkResult], line_plot: bool = True):
             y = np.mean(np.vstack(values), axis=0)[5:]
             x = np.arange(1, len(y) + 1, 1)
 
-            ax.plot(x, smooth(y, 20), label=name)
+            ax.plot(x, smooth(y, 20), label=name, linewidth=2)
 
         ax.legend(loc='upper left')
         ax.set_xlabel('Iteration')
@@ -191,35 +172,39 @@ def plot_openml_100(persistence: MongoPersistence):
         'Random Search': [],
         'Grid Search': [],
         'SMAC': [],
-        'BOHB': [],
-        'Optunity': [],
         'hyperopt': [],
-        'RoBo gp': [],
+        'BOHB': [],
+        'RoBO': [],
+        'Optunity': [],
         'BTB': []
     }
+    ids = []
     for id in tasks:
-        print(id)
+
         benchmark = OpenMLBenchmark(id, load=False)
 
         d = {}
         results = persistence.load_all(benchmark)
+
         for res in results:
+            valid = any([solver.score < 1 for solver in res.solvers])
+            if valid:
+                ids.append(id)
+
             for solver in res.solvers:
+
                 y = solver.as_numpy()[1]
 
-                if solver.algorithm == 'Random Search' or solver.algorithm == 'Grid Search':
-                    y += 0.05
-                if solver.algorithm == 'Optunity':
-                    y += 0.04
+                key = solver.algorithm
+                if key == 'RoBo gp':
+                    key = 'RoBO'
 
-                d.setdefault(solver.algorithm, []).append(y)
+                d.setdefault(key, []).append(y)
 
         for key, value in d.items():
             for list in value:
                 if np.mean(list) < 1:
                     ls[key].append(list[0:325])
-
-    ls['RoBo'] = ls.pop('RoBo gp')
 
     fig, ax = plt.subplots()
     fig.set_size_inches(20, 8)
@@ -227,9 +212,10 @@ def plot_openml_100(persistence: MongoPersistence):
 
     for key, value in ls.items():
         y = np.mean(np.vstack(value), axis=0)
-        print('{}: {}'.format(key, y[-1]))
+        std = np.std(np.vstack(value), axis=0)
+        print('{}: {:.4f} \\pm {:.4f}'.format(key, y[200], std[200]))
 
-        ax.plot(np.arange(1, len(y) + 1, 1), y, label=key)
+        ax.plot(np.arange(1, len(y) + 1, 1), y, label=key, linewidth=2.0)
 
     # ax.set_yscale('log')
     ax.set_xscale('log')
@@ -279,28 +265,32 @@ def plot_branin():
 
 
 def plot_successive_halving():
-    plt.plot([0, 0.125, 0.25, 0.5, 1], [1, 0.40, 0.27, 0.2, 0.16])
+    fig, ax = plt.subplots()
+    fig.set_size_inches(10, 7)
+    fig.set_dpi(250)
 
-    plt.plot([0, 0.125, 0.25], [1, 0.65, 0.5])
-    plt.plot([0, 0.125, 0.25], [1, 0.55, 0.40])
+    ax.plot([0, 0.125, 0.25, 0.5, 1], [1, 0.40, 0.27, 0.2, 0.16])
 
-    plt.plot([0, 0.125, 0.25, 0.5], [1, 0.45, 0.25, 0.22])
+    ax.plot([0, 0.125, 0.25], [1, 0.65, 0.5])
+    ax.plot([0, 0.125, 0.25], [1, 0.55, 0.40])
 
-    plt.plot([0, 0.125], [1, 0.9])
-    plt.plot([0, 0.125], [1, 0.85])
-    plt.plot([0, 0.125], [1, 0.76])
-    plt.plot([0, 0.125], [1, 0.68])
+    ax.plot([0, 0.125, 0.25, 0.5], [1, 0.45, 0.25, 0.22])
+
+    ax.plot([0, 0.125], [1, 0.9])
+    ax.plot([0, 0.125], [1, 0.85])
+    ax.plot([0, 0.125], [1, 0.76])
+    ax.plot([0, 0.125], [1, 0.68])
 
     # Budget Constraints
-    plt.plot([0.125, 0.125], [0, 1], c='k')
-    plt.plot([0.25, 0.25], [0, 1], c='k')
-    plt.plot([0.5, 0.5], [0, 1], c='k')
-    plt.plot([1, 1], [0, 1], c='k')
+    ax.plot([0.125, 0.125], [0, 1], c='k')
+    ax.plot([0.25, 0.25], [0, 1], c='k')
+    ax.plot([0.5, 0.5], [0, 1], c='k')
+    ax.plot([1, 1], [0, 1], c='k')
 
-    plt.ylim([0, 1])
-    plt.xlim([0, 1])
-    plt.ylabel('Loss')
-    plt.xlabel('Budget')
-    plt.xticks([0.125, 0.25, 0.5, 1.0], ['12.5%', '25%', '50%', '100%'])
+    ax.set_ylim([0, 1])
+    ax.set_xlim([0, 1])
+    ax.set_ylabel('Loss')
+    ax.set_xlabel('Budget')
+    ax.set_xticks([0.125, 0.25, 0.5, 1.0], ['12.5%', '25%', '50%', '100%'])
 
     plt.savefig('evaluation/plots/successive_halving.pdf', bbox_inches='tight')
